87828bfe14bed11bbd0ab3c653c5593ae7ad58de
Sabrina Robinson <sabrinarob@gmail.com>
2018-12-22 15:49:16 -0700

    Dries up code for client call

--- app/client/starwars_client.rb
+++ app/client/starwars_client.rb
@@ -3,14 +3,17 @@ class StarwarsClient
     @connection = Faraday.new(url: 'https://swapi.co')
   end
 
+
   def get_vehicles
-    response = @connection.get('/api/vehicles/')
-    response.body
+    use_the_force('/api/vehicles/')
   end
 
   def get_vehicle(ship)
-    response = @connection.get("/api/vehicles/#{ship}/")
-    # binding.pry
-    response.body
+    use_the_force("/api/vehicles/#{ship}/")
   end
+
+  private
+    def use_the_force(url)
+      @connection.get(url).body
+    end
 end
