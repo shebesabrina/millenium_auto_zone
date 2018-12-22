7d59634e502b7e20202b122c3836486a28a2173e
Sabrina Robinson <sabrinarob@gmail.com>
2018-12-22 15:51:14 -0700

    Dries up code for client call in controller

--- app/controllers/api/v1/spacecrafts_controller.rb
+++ app/controllers/api/v1/spacecrafts_controller.rb
@@ -1,12 +1,10 @@
 class Api::V1::SpacecraftsController < ApplicationController
 
   def index
-    client = StarwarsClient.new
     render json: client.get_vehicles
   end
 
   def show
-    client = StarwarsClient.new
     render json: client.get_vehicle(params[:id])
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def client
+    StarwarsClient.new
+  end
 end
