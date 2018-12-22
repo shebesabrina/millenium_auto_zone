4a3ee44fdaecaba85d50b162dc057ff9b6100819
Sabrina Robinson <sabrinarob@gmail.com>
2018-12-22 15:30:02 -0700

    TEST - Testing show view

--- spec/requests/api/v1/spacecraft_request_spec.rb
+++ spec/requests/api/v1/spacecraft_request_spec.rb
@@ -6,8 +6,19 @@ describe "Spacecraft API" do
     get '/api/v1/spacecrafts/'
 
     expect(response).to be_successful
-    ship = JSON.parse(response.body)
-    expect(ship["count"]).to eq(39)
+    ships = JSON.parse(response.body)
+    expect(ships["count"]).to eq(39)
+  end
+
+  it "displays one ship" do
+    ship_1 = JSON.parse(File.read(Rails.root.join("spec", "fixtures", "vehicle_4.json")))
+
+    get '/api/v1/spacecrafts/4/'
+
+    expect(response).to be_successful
+    response = JSON.parse(response.body)
+    expect(ship_1).to eq(response)
+    expect(response.count).to eq(1)
   end
 
 end
